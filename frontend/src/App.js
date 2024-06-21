c5ceaa772e418c4d3c6cbea5e43c403900667d02
Parvatisali <parvatisali429@gmail.com>
2024-06-21 22:32:14 +0530

    adding services pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Services from "./pages/Services";
 
 function App() {
   return (
@@ -15,8 +16,10 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           <Route path="/Login" element={<Login />}></Route>
           <Route path="/signUp" element={<SignUp />}></Route>
+          <Route path="/Services" element={<Services />}></Route>
         </Routes>
       </BrowserRouter>
+      {/* <Services /> */}
     </div>
   );
 }
